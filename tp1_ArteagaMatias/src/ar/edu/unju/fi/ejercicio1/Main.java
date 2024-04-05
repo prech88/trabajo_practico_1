35ce11c778bf4def4d98f9e1b5f62b653cfb674a
Gise <Gise@DESKTOP-M7AU8KI>
2024-04-05 14:36:47 -0300

    Se agrega paquete y clase del punto 1

---
+++ tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1;
+		int num2;
+		int num3;
+		int num4;
+		int num5;
+		
+		num1 = 7;
+		num2 = 23;
+		num3 = 5;
+		num4 = 8;
+		num5 = 34;
+		
+		double promedio;
+		promedio = (num1+num2+num3+num4+num5)/5;
+		System.out.println("El promedio es: "+promedio);
+
+	}
+
+}
