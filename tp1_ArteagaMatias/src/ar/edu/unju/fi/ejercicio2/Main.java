41179bedf9fb2d27d7cc040b346513511601af70
Gise <Gise@DESKTOP-M7AU8KI>
2024-04-05 16:58:04 -0300

    Paquete del punto 2 del trabajo practico 1

---
+++ tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,23 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombre;
+		int edad;
+		int alturaEdificio;
+		Float precioProducto;
+		String numeroTelefono;
+		Double coseno;
+		
+		nombre = "Matias";
+		edad = 34;
+		alturaEdificio = 87;
+		precioProducto = 109.99f;
+		numeroTelefono = "3884578659";
+		coseno = 0.87758256189;
+		
+		System.out.println(nombre+"\n"+edad+"\n"+alturaEdificio+"\n"+precioProducto+"\n"+numeroTelefono+"\n"+coseno);
+	}
+
+}
