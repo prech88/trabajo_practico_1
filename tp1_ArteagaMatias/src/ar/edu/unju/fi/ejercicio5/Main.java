10604f476bae8061256fef6f7cedc85fbc488092
Gise <Gise@DESKTOP-M7AU8KI>
2024-04-05 18:11:39 -0300

    paquete 5 con el punto 5 del trabajo practico 1

---
+++ tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.fi.ejercicio5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numero;
+		int resultado;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese numero entero: ");
+		
+		do {
+			numero = scanner.nextInt();
+			if(numero <1 || numero>9) {
+				System.out.println("Por favor vuelva a ingresar un numero entre 1 y 9");
+			}
+		}while(numero <1 || numero>9 );
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println(numero+" x "+i+" = "+numero*i);
+		}
+		scanner.close();
+		
+	}
+
+}
