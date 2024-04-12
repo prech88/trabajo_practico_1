f0d234afb8da712aa0931e372fd996c82e31ab98
Matias <Matias@DESKTOP-5683VC2>
2024-04-12 18:38:40 -0300

    Arreglo en ejercicio 5, se borra una variable no usada

--- tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio5/Main.java
+++ tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio5/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		int numero;
-		int resultado;
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Ingrese numero entero: ");
 		
