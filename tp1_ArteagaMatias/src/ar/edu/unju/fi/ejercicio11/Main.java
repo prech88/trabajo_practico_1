a1432232842f5f51d617868edd56dae3d35c1cc1
Matias <Matias@DESKTOP-5683VC2>
2024-04-13 01:37:19 -0300

    Asistencia para la clase practica de 12/04/2024 y subida del paquete con la resolucion del ejercicio 11 de la segunda parte del tp1

---
+++ tp1_ArteagaMatias/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+        for(int j=40,i=2;j>0;j--,i++) {
+        	System.out.print(j*i+" ");
+        }
+	}
+
+}
